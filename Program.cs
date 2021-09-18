e78b775cdb7c8561a95d3ebc72988e8220978cc3
franciny arielly <francinyalexandre07@gmail.com>
2021-09-18 15:03:01 -0300

    resolução exercicio musica

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace musica
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.ResetColor();
+            Console.BackgroundColor=ConsoleColor.White;
+            Console.ForegroundColor=ConsoleColor.Black;
+    
+            Console.WriteLine("auto reverse - O rappa");
+            Console.WriteLine("Uma doce familia");
+            Console.WriteLine("que tem am mania");
+            Console.WriteLine("de achar alegria");
+            Console.WriteLine("Motivo e razão");
+            Console.WriteLine("Onde dizem que não");
+            Console.WriteLine("Aí que tá a mágica, meu irmão");
+            Console.ResetColor();
+        }
+    }
+}
